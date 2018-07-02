a8a29e032832fab5c69d8bece6bd8efccf940996
gyoomi <gyoomi0709@foxmail.com>
2018-07-02 23:02:03 +0800

    集成JWT初始化版本

---
+++ src/main/java/com/cherry/framework/constant/JWTConstant.java
@@ -0,0 +1,16 @@
+/**
+ * Copyright © 2018, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.cherry.framework.constant;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/7/2 22:59
+ */
+public class JWTConstant {
+}
