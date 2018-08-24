8d7fa24e9db111298a1dd59dafec0a1f1a753c7e
gyoomi <gyoomi0709@foxmail.com>
2018-08-24 11:06:36 +0800

    添加扫描包，提升扫描效率

--- src/main/java/com/cherry/framework/FrameworkApplication.java
+++ src/main/java/com/cherry/framework/FrameworkApplication.java
@@ -5,9 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * 系统配置基类
+ *
+ * @author Leon
+ * @version 2018/5/23 14:40
+ */
 @SpringBootApplication
 @MapperScan(value = "com.cherry.framework.dao")
+@ComponentScan(basePackages = "com.cherry.framework")
 public class FrameworkApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
