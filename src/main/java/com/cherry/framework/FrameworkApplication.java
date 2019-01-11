de884491ca613f168f260ecdfd4e43872090acff
JiaxiLee <jiaxiwuli@126.com>
2019-01-11 10:48:13 +0800

    提交branch

--- src/main/java/com/cherry/framework/FrameworkApplication.java
+++ src/main/java/com/cherry/framework/FrameworkApplication.java
@@ -30,6 +30,10 @@ public class FrameworkApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         lg.info("Framework start at 【{}】", LocalDateTime.now());
+
+
+
+
         SpringApplication.run(FrameworkApplication.class, args);
     }
 
