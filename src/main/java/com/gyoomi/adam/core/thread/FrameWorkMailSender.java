214c424d117d8d4cbb15b8a23fa4ba037051c7a9
gyoomi <gyoomi0709@foxmail.com>
2019-10-22 17:40:15 +0800

    feat(all): 2.0版本: email

--- src/main/java/com/gyoomi/adam/core/thread/FrameWorkMailSender.java
+++ src/main/java/com/gyoomi/adam/core/thread/FrameWorkMailSender.java
@@ -1,51 +1,49 @@
-///**
-// * Copyright © 2018, LeonKeh
-// * <p>
-// * All Rights Reserved.
-// */
-//
-//package com.cherry.adam.core.thread;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.mail.javamail.JavaMailSender;
-//import org.springframework.mail.javamail.MimeMessageHelper;
-//
-//import javax.mail.internet.MimeMessage;
-//
-///**
-// * 类功能描述
-// *
-// * @author Leon
-// * @version 2018/8/18 23:40
-// */
-//public class FrameWorkMailSender extends BaseTaskEventThread<Mail> {
-//
-//    @Autowired
-//    private JavaMailSender javaMailSender;
-//
-//    @Value("${spring.mail.username}")
-//    private String from;
-//
-//    public FrameWorkMailSender(String name) {
-//        super(name);
-//    }
-//
-//    @Override
-//    public short getWorkers() {
-//        return 2;
-//    }
-//
-//    @Override
-//    public void doWork(Mail task) throws Exception {
-//        if (task != null) {
-//            MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
-//            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
-//            mimeMessageHelper.setFrom(from);
-//            mimeMessageHelper.setSubject(task.getTitle());
-//            mimeMessageHelper.setTo(task.getTo());
-//            mimeMessageHelper.setText(task.getContent());
-//            this.javaMailSender.send(mimeMessage);
-//        }
-//    }
-//}
+/**
+ * Copyright © 2018, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.gyoomi.adam.core.thread;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/8/18 23:40
+ */
+public class FrameWorkMailSender extends BaseTaskEventThread<Mail> {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    private String from;
+
+    public FrameWorkMailSender(String name) {
+        super(name);
+    }
+
+    @Override
+    public short getWorkers() {
+        return 2;
+    }
+
+    @Override
+    public void doWork(Mail task) throws Exception {
+        if (task != null) {
+            MimeMessage mimeMessage = this.javaMailSender.createMimeMessage();
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setFrom(from);
+            mimeMessageHelper.setSubject(task.getTitle());
+            mimeMessageHelper.setTo(task.getTo());
+            mimeMessageHelper.setText(task.getContent());
+            this.javaMailSender.send(mimeMessage);
+        }
+    }
+}
